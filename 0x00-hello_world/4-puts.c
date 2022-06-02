a03ca7a9ee5e88fc7bd4a84ae80484a1cd33b758
Samson <samsonsimeon2004@gmail.com>
2022-06-02 01:13:05 -0700

    this is the low level programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,10 +9,7 @@
 * main:this is the main function of the program
 */
 
-int main(void)
-/*
-* Main- function is the starting of the code
-*/
+int main(void) /** Main- function is the starting of the code */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
