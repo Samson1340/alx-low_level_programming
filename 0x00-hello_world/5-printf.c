bcda3d55e2ca0c0a4df94b081ce5fccad8f31c1d
Samson <samsonsimeon2004@gmail.com>
2022-06-02 04:04:43 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,11 @@
 #include <stdio.h>
 
 /*
-* main:this is the main function of the program
+* main - this is the main function of the program
+* Return: 0 for main function
 */
 
-int main(void) /* Main- function is the starting of the code */
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
