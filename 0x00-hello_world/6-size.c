0283137337f08cfdf01275832efb7a299f47531e
Samson <samsonsimeon2004@gmail.com>
2022-06-02 01:53:31 -0700

    this is the low level programming commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,5 @@ printf("Size of a int: %zu byte(s)",sizeof(int));
 printf("Size of a long int: %zu byte(s)",sizeof(long int));
 printf("Size of a long long int: %zu byte(s)",sizeof(long long int));
 printf("Size of a float: %zu byte(s)",sizeof(float));
-return(0)
+return(0);
 }
