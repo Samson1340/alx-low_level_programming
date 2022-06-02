6d959357207474824669be9b8bceea700799c27a
Samson <samsonsimeon2004@gmail.com>
2022-06-02 01:02:26 -0700

    this is the low level programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 /*                                                                                     
- *the file :4-puts.c                                                                   
- *Auth: Samson                                     
+*the file :4-puts.c                                                                   
+*Auth: Samson                                     
 */   
 #include <stdio.h>
 /*
- *this is the main function of the program
+*this is the main function of the program
 */  
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
