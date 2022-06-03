0dadaaac2c85c19eb6f88cc7fddfb62dbc0e3e7a
Samson <samsonsimeon2004@gmail.com>
2022-06-03 03:11:41 -0700

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+* File - 5-print_numbers.c
+* Author - Samson 
+*/
+
+/**
+* main - the main function of the program
+* Return: 0 for all function
+*/
+
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
