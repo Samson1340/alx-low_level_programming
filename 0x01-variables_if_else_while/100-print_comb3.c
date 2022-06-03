f166baca3a0e2f063c61c7ef7a02a0c89e3bc649
Samson <samsonsimeon2004@gmail.com>
2022-06-03 05:32:13 -0700

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*
+* File - 100-print_comb3.c
+* Author - Samson
+*/
+
+/**
+* main - the main function of the program
+* Return: 0 for all function
+*/
+
+int main(void)
+{
+int n;
+int m;
+for (n = 0; n < 10; n++)
+{
+for (m = n+1; m < 10; m++)
+{
+putchar((n % 10) + '0');
+putchar((m % 10) + '0');
+if (n == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
