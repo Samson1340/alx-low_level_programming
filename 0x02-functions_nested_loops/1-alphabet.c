ffa35a94fcb8f87c486bf6feaab81ae55d1c97c5
Samson <samsonsimeon2004@gmail.com>
2022-06-07 03:57:32 -0700

    this functions nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+* File - the function print alphabet
+* Author - Samson
+*/
+
+/**
+* print_alphabet - prints the alphabet
+* Return: 0 for the function
+*/
+
+void print_alphabet(void)
+{
+  char ch = 'a';
+  for (ch = 'a'; ch <= 'z'; ch++)
+    {
+      putchar(ch);
+    }
+  putchar('\n');
+  return (0);
+}
