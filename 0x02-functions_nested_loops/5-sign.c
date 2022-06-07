78f6651b1053ffc1d85e3b1b9a148019bdff8380
Samson <samsonsimeon2004@gmail.com>
2022-06-07 14:49:59 -0700

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_sign - the function
+* @n: Decelartion
+* Return: 0 and 1
+*/
+
+int print_sign(int n)
+{
+if(n < 0)
+return ('-');
+else if (n > 0)
+return ('+');
+else
+return (0);
+}
