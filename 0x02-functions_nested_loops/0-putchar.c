1d23732540eaa33d8908719b1b3bc744e83a9a90
Samson <samsonsimeon2004@gmail.com>
2022-06-07 03:04:08 -0700

    this functions nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/*
+* File - 0-putchar.c
+* Author - Samson
+*/
+/**
+* main - the main function print putchar
+* Return: 0 for the all function 
+*/
+
+int main(void)
+{
+  putchar("_putchar");
+  return(0);
+}
