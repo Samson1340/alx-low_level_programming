8bcb36de482ab29ee21cd42a736e0855ac3f50ad
Samson <samsonsimeon2004@gmail.com>
2022-06-07 12:51:25 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,10 +10,10 @@
 */
 void print_alphabet(void)
 {
-  char ch = 'a';
-  for (ch = 'a'; ch <= 'z'; ch++)
-    {
-      _putchar(ch);
-    }
-  _putchar('\n');
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
 }
