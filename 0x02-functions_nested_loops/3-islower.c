5594c5fe387028bd6ec1a841d2d874f54c602f4e
Samson <samsonsimeon2004@gmail.com>
2022-06-07 13:59:02 -0700

    update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*
+* File - int _islower(int c)
+* Author - Samson
+*/
+
+/**
+* int _islower - check lowercase 
+* Return: 0 if it is not 1 when it is lowercase
+*/
+
+int _islower(int c)
+{
+char c = 'c';
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch == c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+}
