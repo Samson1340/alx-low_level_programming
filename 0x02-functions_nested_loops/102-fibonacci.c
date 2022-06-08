baf6b4327bf61bb3d4745c60e920dd8129274a60
Samson <samsonsimeon2004@gmail.com>
2022-06-08 07:33:39 -0700

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,8 +13,8 @@ int count;
 int num1 = 0, num2 = 1, sum;
 for (count = 0; count < 50; count++)
 {
-sum = num11 +num2;
-printf("%lu", sum);
+sum = num1 +num2;
+printf("%d", sum);
 num1 = num2;
 num2 = sum;
 if (count == 49)
