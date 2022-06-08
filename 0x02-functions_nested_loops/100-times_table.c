e7f3176f48247d1f1dde011d8088a9ff9e94f12e
Samson <samsonsimeon2004@gmail.com>
2022-06-08 06:15:32 -0700

    update

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_times_table - the function 
+* @n: the number
+* @num1: number1
+* @num2: number2
+* @mult: multiple
+* Return: 0 and 1
+*/
+
+void print_times_table(int n)
+{
+int num1;
+int num2;
+int mult;
+if (n < 15 && n > 0)
+{
+for (num1 = 0; num1 <= n; num1++)
+{
+for (num2 = 0; num2 <= n; num2++)
+{
+mult = num1 * num2;
+printf("%d, ", mult);
+}
+printf("\n");
+}
+}
+}
