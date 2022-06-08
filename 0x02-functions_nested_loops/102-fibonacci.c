8da759da33c5f655895d1a27cd697e73151b5109
Samson <samsonsimeon2004@gmail.com>
2022-06-08 07:29:14 -0700

    update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* main - the main function 
+* @num1: number one 
+* @num2: number two 
+* @sum: the sum 
+* @count: the count
+*/
+
+int main(void)
+{
+int count;
+int num1 = 0, num2 = 1, sum;
+for (count = 0; count < 50; count++)
+{
+sum = num11 +num2;
+printf("%lu", sum);
+num1 = num2;
+num2 = sum;
+if (count == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
