409264394557828c52edf72790dde4e65ccb2267
Samson <samsonsimeon2004@gmail.com>
2022-06-08 07:59:45 -0700

    update

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - the main function
+* Return: 0 and 1
+*/
+int main(void)
+{
+float total;
+unsigned long num1 = 0, num2 = 1, sum;
+while (1)
+{
+sum = num1 + num2;
+if (sum > 4000000)      
+break;      
+if ((sum % 2) == 0)
+total = total + sum;
+num1 = num2;
+num2 = sum;
+}
+printf("%.0f\n", total);
+return (0);
+}             
