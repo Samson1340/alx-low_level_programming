374275c58c166aac0205e1c7463a9331860db0d3
Samson <samsonsimeon2004@gmail.com>
2022-06-08 08:15:58 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,13 +8,13 @@ int main(void)
 int count;
 unsigned long num1 = 1, num2 = 2, sum;
 printf("%lu, %lu, ", num1, num2);
-for (count = 0; count < 98; count++)
+for (count = 0; count < 97; count++)
 {
 sum = num1 + num2;
 printf("%lu", sum);
 num1 = num2;
 num2 = sum;
-if (count == 98)
+if (count == 97)
 printf("\n");
 else
 printf(", ");
