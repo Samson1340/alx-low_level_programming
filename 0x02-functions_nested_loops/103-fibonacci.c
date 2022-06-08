3b15f13013f48a6f3f16010eb9061c2300e7db31
Samson <samsonsimeon2004@gmail.com>
2022-06-08 08:01:42 -0700

    update

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,8 +10,8 @@ unsigned long num1 = 0, num2 = 1, sum;
 while (1)
 {
 sum = num1 + num2;
-if (sum > 4000000)      
-break;      
+if (sum > 4000000)
+break;
 if ((sum % 2) == 0)
 total = total + sum;
 num1 = num2;
@@ -19,4 +19,4 @@ num2 = sum;
 }
 printf("%.0f\n", total);
 return (0);
-}             
+}
