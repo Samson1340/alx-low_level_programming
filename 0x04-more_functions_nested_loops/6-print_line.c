d6c818f5269aa339ce47418d60e3b5dc8be4ba83
Samson <samsonsimeon2004@gmail.com>
2022-06-09 07:05:04 -0700

    this is more function project

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_line - the function to print line
+*/
+
+void print_line(int n)
+{
+int ch;
+for(ch = 1; ch <= n; ch++)
+{
+putchar('_');
+}
+putchar('\n');
+}
