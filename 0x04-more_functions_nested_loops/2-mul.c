db05e558791606a526717115da03b32b5b57b603
Samson <samsonsimeon2004@gmail.com>
2022-06-09 04:57:58 -0700

    this is more function project

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,10 @@
 * mul - A function that multiplies two no
 * @a: the first number
 * @b: the second number
-* Return: 0 and 1
+* Return: the result of the multiplication
 */
 
 int mul(int a, int b)
 {
 return (a * b);
-}                  
+}
