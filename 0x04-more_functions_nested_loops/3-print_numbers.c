b295f16da908355cffb511cfddd68cb28654e036
Samson <samsonsimeon2004@gmail.com>
2022-06-09 05:09:31 -0700

    this is more function project

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - the display 0 to 9
+* @n: the first number
+*/
+
+void print_numbers(void)
+{
+int n;
+for(n = 0; n <= 9; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
