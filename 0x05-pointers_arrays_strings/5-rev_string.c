23a7ad14c7086061bb856fbab3e901e6d9f1f5af
Samson <samsonsimeon2004@gmail.com>
2022-06-13 12:23:08 -0700

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,17 +10,14 @@ void rev_string(char *s)
 {
 int length = 0;
 for (length = 0; *s != '\0'; length++)
-{
 s++;
-}
 s--;
 
 for (length = length; length != 0; length--)
 {
 putchar(*s);
-}
 s--;
-
+}
 putchar('\n');
 }
                                
