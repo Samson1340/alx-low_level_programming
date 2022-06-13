3b8aa493f114fe1b308bce3bc5fe9a3730d77688
Samson <samsonsimeon2004@gmail.com>
2022-06-13 07:04:45 -0700

    update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* rev_string - to print the rev
+* @s: the pointe
+*/
+
+void rev_string(char *s)
+{
+int length = 0;
+for (length = 0; *s != '\0'; length++)
+s++;
+
+s--;
+
+for (length = length; length != 0; length--)
+{
+putchar(*s);
+s--;
+}
+}
+                               
