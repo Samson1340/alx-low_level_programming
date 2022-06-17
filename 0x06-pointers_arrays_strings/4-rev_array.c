413286425b3d2392977e8e3a89a78bddc363ac2e
Samson <samsonsimeon2004@gmail.com>
2022-06-17 07:50:36 -0700

    update

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reverse_array - reverse array
+* @a: the pointer
+* @n: number of byte
+* Return: the copied string
+*/
+
+void reverse_array(int *a, int n)
+{
+int i;
+for (i = n - 1; i >= 0; i--)
+{
+return (a[i]);
+}
