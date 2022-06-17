96694d2fa13a6288384440404bd947ce1fce0469
Samson <samsonsimeon2004@gmail.com>
2022-06-17 08:01:49 -0700

    update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,11 +12,11 @@ void reverse_array(int *a, int n)
 {
 int x[1000];
 int i;
-for (i = 0; i < n; i++)
+for (i = 0; i <= n; i++)
 {
 x[i] = a[i];
 }
-for (i = 0; i < n; i++)
+for (i = 0; i <= n; i++)
 {
 a[i] = x[n - 1];
 }          
